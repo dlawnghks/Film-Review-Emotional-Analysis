754c9ca518ce0f4c487a1fa4ca05bfab9a904166
JuhwanLim <juhwan1120@naver.com>
2024-12-11 22:03:38 +0900

    미완성본, 오류 해결중

---
+++ src/evaluate.py
@@ -0,0 +1,25 @@
+from sklearn.metrics import accuracy_score, classification_report
+import torch
+from preprocess import preprocess_data
+from load_data import load_data
+
+def evaluate_model(test_loader, model, device):
+    model.eval()
+    predictions, true_labels = [], []
+
+    with torch.no_grad():
+        for batch in test_loader:
+            inputs, labels = batch
+            inputs = {key: val.to(device) for key, val in inputs.items()}
+            labels = labels.to(device)
+
+            outputs = model(**inputs)
+            logits = outputs.logits
+            preds = torch.argmax(logits, dim=1)
+
+            predictions.extend(preds.cpu().numpy())
+            true_labels.extend(labels.cpu().numpy())
+
+    acc = accuracy_score(true_labels, predictions)
+    print(f"Accuracy: {acc}")
+    print(classification_report(true_labels, predictions))
