db03de7ebd5f822bb7aa59186a2a0b2d98dd6b2a
JuhwanLim <juhwan1120@naver.com>
2024-12-26 21:30:00 +0900

    로컬에 영어로 평가 작성시 판별

--- src/test.py
+++ src/test.py
@@ -1,26 +1,29 @@
 import torch
 from transformers import AutoModelForSequenceClassification, AutoTokenizer
 
-model_path = "saved_model_multilingual"
+model_path = "saved_model"  # 학습한 모델 경로
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 model = AutoModelForSequenceClassification.from_pretrained(model_path)
 tokenizer = AutoTokenizer.from_pretrained(model_path)
 model.to(device)
 model.eval()
 
-print("Enter a review to predict sentiment (type 'exit' to quit):")
+print("Sentiment Analysis is ready! Enter a review to predict sentiment (type 'exit' to quit):")
+
 while True:
     review = input("Your review: ")
-    if review.lower() == "exit":
+    if review.lower() == "exit":  
         print("Exiting sentiment analysis...")
         break
-    
-    encodings = tokenizer(review, truncation=True, padding=True, max_length=32, return_tensors="pt")
+
+    encodings = tokenizer(review, truncation=True, padding=True, max_length=64, return_tensors="pt")
     inputs = {key: val.to(device) for key, val in encodings.items()}
     with torch.no_grad():
         outputs = model(**inputs)
         logits = outputs.logits
         prediction = torch.argmax(logits, dim=1).item()
         sentiment = "긍정적" if prediction == 1 else "부정적"
-        print(f"Review: {review}\nPredicted Sentiment: {sentiment}\n")
+
+    print(f"\nReview: {review}\nPredicted Sentiment: {sentiment}\n")
